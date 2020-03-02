7aeed491b4d9f73d2f28feb1e9167e107b2923a1
Dayle Kotturi <karendayle@gmail.com>
2020-03-02 14:03:16 -0600

    new version

--- mcraman/drawCylinder.m
+++ mcraman/drawCylinder.m
@@ -1,8 +1,8 @@
 % ref https://www.mathworks.com/matlabcentral/answers/486172-how-can-i-plot-a-filled-cylinder-with-a-specific-height
 % Values for 2 cylinders
-r = [1, 1];             % radii of each cyl
-cnt = [1,2; 3,5];       % [x,y] center of each cyl
-height = [2,8];         % height of each cyl
+r = [0.05, 1];             % radii of each cyl
+cnt = [1,0.2; 3,5];       % [x,y] center of each cyl
+height = [1,8];         % height of each cyl
 color = [1 0 0; 0 .5 0];% color of each cyl
 nSides = 100;           % number of "sides" of the cyl
 % Create figure
@@ -19,7 +19,8 @@ function [h1, h2, h3] = plotCylinderWithCaps(r,cnt,height,nSides,color)
 capColor = [0., 0., 0.]; % black
 X = X + cnt(1); 
 Y = Y + cnt(2); 
-Z = Z * height; 
+%Z = Z * height; 
+Z = Z* height-1*height/2; 
 h1 = surf(X,Z,Y,'facecolor',color,'LineStyle','none');
 h2 = fill3(X(1,:),Z(1,:),Y(1,:),capColor);
 h3 = fill3(X(2,:),Z(2,:),Y(2,:),capColor);
